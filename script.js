5788ff34135b1f319233a7fcfdcd9abc37ef17e4
Juan Carrillo <juan@juanca.org>
2022-09-28 17:27:59 -0500

    feat(script): reset form and disable submit button on load

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello! 👋");
+const cardForm = document.querySelector(".card__form");
+const submitButton = document.getElementById("card__form--submit-button");
+
+// Reset cardForm and disable submitButton on load.
+window.addEventListener("load", () => {
+  cardForm.reset();
+  submitButton.disabled = true;
+});
