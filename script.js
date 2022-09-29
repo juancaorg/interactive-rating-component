8cef56eda7b108b4192d2d8d523ee16692eaa92a
Juan Carrillo <juan@juanca.org>
2022-09-29 14:35:37 -0500

    feat(script): add function to enable submit button after radio button is selected and save selected value

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const cardForm = document.querySelector(".card__form");
 const submitButton = document.getElementById("card__form--submit-button");
+const scoreButtons = document.querySelectorAll("[type='radio']");
+let selectedValue = 0;
+
+// After a radio button is selected,
+// enable submit button and save the selected value.
+scoreButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    submitButton.disabled = false;
+    selectedValue = button.value;
+  });
+});
 
 // Reset cardForm and disable submitButton on load.
 window.addEventListener("load", () => {
