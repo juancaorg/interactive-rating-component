65130fbfc8ca5d3a65e35dbca417ded18e60f098
Juan Carrillo <juan@juanca.org>
2022-09-29 15:04:40 -0500

    feat: add submit rating function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const ratingCard = document.getElementById("rating__card");
+const thanksCard = document.getElementById("thanks__card");
+const thanksRating = document.querySelector(".thanks__rating");
 const cardForm = document.querySelector(".card__form");
 const submitButton = document.getElementById("card__form--submit-button");
 const scoreButtons = document.querySelectorAll("[type='radio']");
@@ -10,6 +13,18 @@ scoreButtons.forEach((button) => {
     submitButton.disabled = false;
     selectedValue = button.value;
   });
+});
+
+// After submitting your rating,
+// pass the rating to the thanksRating span,
+// hide the ratingCard and make the thanksCard visible.
+cardForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (selectedValue !== 0) {
+    thanksRating.textContent = selectedValue;
+    ratingCard.classList.toggle("hidden");
+    thanksCard.classList.toggle("hidden");
+  }
 });
 
 // Reset cardForm and disable submitButton on load.
